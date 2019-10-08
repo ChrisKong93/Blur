e4023c716c7e682ebd62c0123cf9b445baa12c4e
ChrisKong <609027949@qq.com>
2019-10-08 17:13:46 +0800

    Update blur.cpp
    精简代码

--- cpp/cpu/blur.cpp
+++ cpp/cpu/blur.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int main() {
 	cout << "Hello World!\n" << endl;
-	// 读入一张图片（poyanghu缩小图）    
-	Mat img = imread("C:\\Users\\Lenovo\\Desktop\\image\\timg.jfif");
+	// 读入一张图片
+	Mat img = imread("../../../image/test.jfif");
 	//Mat img = imread("C:\\Users\\Lenovo\\Desktop\\image\\test.jpg");
 	clock_t startTime, endTime;
 	
@@ -43,13 +43,7 @@ int main() {
 					}
 				}
 				img_new.at<Vec3b>(i, j) = s / (k*k);
-				//cout << s << endl;
 			}
-			//cout << s.at<Vec3b>(0, 0) << endl;
-			//return 0;
-			//img_new.at<Vec3b>(i, j)[0] = img.at<Vec3b>(i, j)[0] / 2;
-			//img_new.at<Vec3b>(i, j)[1] = img.at<Vec3b>(i, j)[1] / 2;
-			//img_new.at<Vec3b>(i, j)[2] = img.at<Vec3b>(i, j)[2] / 2;
 		}
 	}
 	endTime = clock();//计时结束
@@ -58,19 +52,7 @@ int main() {
 	namedWindow("图片");
 	// 在窗口中显示图片   
 	imshow("图片", img_new);
-	// 等待6000 ms后窗口自动关闭    
+	// 等待窗口不关闭
 	waitKey(0);
 	return 0;
 }
-
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
